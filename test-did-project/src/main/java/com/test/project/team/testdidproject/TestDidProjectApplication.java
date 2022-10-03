66a47e77f9dcabc6700e7c626e35b70303568d9c
hone_book <sangcom12@naver.com>
2022-10-03 18:14:47 +0900

    first commit

---
+++ test-did-project/src/main/java/com/test/project/team/testdidproject/TestDidProjectApplication.java
@@ -0,0 +1,13 @@
+package com.test.project.team.testdidproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDidProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDidProjectApplication.class, args);
+	}
+
+}
---
+++ test-did-project/src/test/java/com/test/project/team/testdidproject/TestDidProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.project.team.testdidproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestDidProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
